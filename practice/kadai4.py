09818f78f23152957b85b4be108799345e996957
Wakasugi Yuya <102941311+wakasugi1765@users.noreply.github.com>
2022-12-22 02:08:49 +0900

    update kadai4.py and add def show_delta

--- practice/kadai4.py
+++ practice/kadai4.py
@@ -72,25 +72,94 @@ def equation09(t):
 def equation10(t):
     return np.exp(-t)*np.sin(t)*step(t)
 
-def show_graph(x,y):
+def show_graph(x, y, pi=False):
+    fontsize = plt.rcParams["font.size"]
+    plt.rcParams["font.size"] = 20
+    fig = plt.figure(figsize=(16.0, 9.0), dpi=120) # FulHD(1920*1080)
+    #xticks, xticklabels = get_xticks(x)
+    if pi:
+        xlim = (x[0]-0.1, x[-1]+0.1)
+        xticks, xticklabels = get_xticks_pi(x)
+    else:
+        st, et = int(x[0]), int(x[-1])
+        xlim=(st-0.1, et+0.1)
+        xticks=np.linspace(st, et, et-st+1)
+        xticklabels = xticks
+    ylim, yticks = get_ylim(y)
+    ax  = fig.add_subplot(
+        1,1,1,
+        xlabel='t', xlim=xlim, xticks=xticks, xticklabels=xticklabels,
+        ylabel='y(t)', ylim=ylim, yticks=yticks
+        )
+    # ax.legend(fontsize=20)
+
     plt.plot(x,y)
+    plt.tight_layout()
+    plt.grid()
     plt.show()
+    plt.rcParams["font.size"] = fontsize
     pass
 
+def show_delta(*x):
+    fontsize = plt.rcParams["font.size"]
+    plt.rcParams["font.size"] = 20
+    fig = plt.figure(figsize=(16.0, 9.0), dpi=120) # FulHD(1920*1080)
+    st, et = -5, 5
+    ax  = fig.add_subplot(
+        1,1,1,
+        xlabel='t', xlim=(st,et), xticks=np.linspace(st, et, et-st+1),
+        ylabel='x(t)', ylim=(-0.1,1.1), yticks=[0,1], yticklabels=[0, '∞']
+        )
+    # ax.legend(fontsize=20)
+
+    # plt.plot(x,y)
+    
+    for xi in x:
+        plt.scatter(xi, 0, marker='o', color='b', s=100)
+        plt.scatter(xi, 1, marker='^', color='b', s=100)
+        plt.plot([xi,xi], [0,1], color='b')
+        
+    # plt.plot([1,1], [0,1])
+    # plt.plot([2,2], [0,2])
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
+    plt.rcParams["font.size"] = fontsize
+    pass
+
+def get_ylim(y):
+    max_val = np.abs(y).max()
+    y_max = 0.25
+
+    while max_val >= y_max:
+        print(max_val, y_max)
+        y_max += 0.25
+    ylim = (-y_max-0.1, y_max+0.1)
+    yticks = np.linspace(-y_max, y_max, int((2*y_max)/0.25)+1)
+    
+    return ylim, yticks
+
+def get_xticks_pi(x):
+    st, et = x[0], x[-1]
+    xticks, xticklabels = [], []
+    k = int(np.abs(st)//np.pi)
+    for i in range(-k, k+1):
+        xticks.append(i*np.pi)
+        xticklabels.append('{}π'.format(i))
+        pass
+    return xticks, xticklabels
+
 if __name__ == '__main__':
-    # st, et = -10, 10
     st, et = -10, 10
     t  = np.linspace(st, et, (et-st)*100)
-    # t = np.arange(101)-50
-    # xt = step(t)
-    # xt = equation01(t)
-    # xt = equation02(t)
-    # xt = equation03(t)
-    # xt = equation04(t)
-    # xt = equation05(t)
-    # xt = equation06(t)
-    # xt = equation07(t)
-    # xt = equation08(t)
-    # xt = equation09(t)
-    xt = equation10(t)
-    show_graph(t, xt)
+    # xt, pi = equation01(t), True
+    # xt, pi = equation02(t), False
+    # xt, pi = equation03(t), False
+    # xt, pi = equation04(t), False
+    # xt, pi = equation05(t), False
+    # xt, pi = equation06(t), False
+    xt, pi = equation07(t), False
+    # xt, pi = equation08(t), False
+    # xt = show_delta(1,2)
+    # xt, pi = equation10(t), False
+    show_graph(t, xt, pi)
